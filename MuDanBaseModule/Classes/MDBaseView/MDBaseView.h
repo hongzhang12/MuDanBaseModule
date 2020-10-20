ed61ba5da960a959af4aa64b6310bef119da4841
张洪 <>
2020-10-20 15:16:31 +0800

    0.1.0

---
+++ MuDanBaseModule/Classes/MDBaseView/MDBaseView.h
@@ -0,0 +1,16 @@
+//
+//  MDBaseView.h
+//  MuDanBaseModule
+//
+//  Created by 张洪 on 2020/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MDBaseView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MuDanBaseModule/Classes/MDBaseView/MDBaseView.m
@@ -0,0 +1,20 @@
+//
+//  MDBaseView.m
+//  MuDanBaseModule
+//
+//  Created by 张洪 on 2020/10/20.
+//
+
+#import "MDBaseView.h"
+
+@implementation MDBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
